14acb0ab3dc73460880c95e2186a2e262156c711
mfthfzn <miftahulfauzan55@gmail.com>
2026-05-12 23:44:18 +0800

    feat: profile users

---
+++ front-end-clothify/js/dashboard.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", async function (event) {
+  event.preventDefault();
+
+  try {
+    const response = await fetch(`http://127.0.0.1:8080/api/auth/session`, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (response.status === 200) {
+      const responseData = await response.json();
+
+      const names = responseData.data.name;
+      const role = responseData.data.role;
+      const email = responseData.data.email;
+
+      const profileName = document.querySelector(".profile-name");
+      const profileRole = document.querySelector(".profile-role");
+
+      profileName.textContent = names;
+      profileRole.textContent = role;
+
+      const nameField = document.querySelector("#name");
+      const emailField = document.querySelector("#email");
+      const roleField = document.querySelector("#role");
+
+      if (!nameField || !emailField || !roleField) return;
+
+      nameField.value = names;
+      emailField.value = email;
+      roleField.value = role;
+    } else if (response.status === 401) {
+      const responseRefresh = await fetch(`http://127.0.0.1:8080/api/auth/refresh`, {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if(responseRefresh.status === 200) {
+        window.location.reload()
+      } else {
+        window.location.href = "../login.html"
+      }
+
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    window.location.href = "../login.html";
+  }
+});
